a8fa56f69fffa020494e50e8e676bd63705ca567
imad-bkr <kariimad@yahoo.fr>
2021-05-18 21:44:41 +0200

    commit

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,7 @@
 <header class="header">
     <img class="logo-mini" src="../img/logo.png" alt="EdSide">
     <div class="user">Nom d'utilisateur</div>
-    <ul class="user-menu">
+    <ul class="user-menu hidden">
         <a href=""><li>Paramètres</li></a>
         <a href=""><li>English</li></a>
         <a href=""><li>Déconnexion</li></a>
